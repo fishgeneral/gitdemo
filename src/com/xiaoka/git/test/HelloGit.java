a9d7d873c3d030646bc1cd9536dc32380f14d14c
wangxiaoka <xiaoka@163.com>
2022-06-07 17:45:10 +0800

    fenzhier提交

--- src/com/xiaoka/git/test/HelloGit.java
+++ src/com/xiaoka/git/test/HelloGit.java
@@ -11,5 +11,7 @@ public class HelloGit {
         System.out.println("这是第三个版本");
         System.out.println("这是分支2");
         System.out.println("这是分支2");
+        System.out.println("这是分支2");
+        System.out.println("这是分支2");
     }
 }
