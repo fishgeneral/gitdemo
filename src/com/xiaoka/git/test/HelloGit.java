cfb25d47cb28f586a2416301a6f50849efe088cb
wangxiaoka <xiaoka@163.com>
2022-06-07 16:49:18 +0800

    第二次提交

--- src/com/xiaoka/git/test/HelloGit.java
+++ src/com/xiaoka/git/test/HelloGit.java
@@ -3,5 +3,8 @@ package com.xiaoka.git.test;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("这是第二个版本");
+        System.out.println("这是第二个版本");
+        System.out.println("这是第二个版本");
     }
 }
